3e35d23d7dd715b06af9cca602414d7bb0c8049b
Rocio-Gauna <rociogauna.3020@gmail.com>
2026-09-23 16:50:42 +0200

    .

---
+++ 2do/Relacion-2/PrepararCarpetas.java
@@ -0,0 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class PrepararCarpetas {
+    
+}
